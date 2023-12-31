274170af501b51d06b22ee5baa067815da2299fe
Tamim-369 <155313411+Tamim-369@users.noreply.github.com>
2023-12-31 10:25:41 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-
-cp -r polybar ~/.config/
+mkdir ~/.config/polybar
+cp -r hack ~/.config/polybar
 cp -r i3 ~/.config/
 cp bg2.jpg ~/Pictures/
 mkdir .themes
 cd .themes
 git clone https://github.com/EliverLara/Nordic.git
 cd ../
+sudo apt install curl espeak
+curl -sS https://starship.rs/install.sh | sh
+cp starship.toml ~/.config/
