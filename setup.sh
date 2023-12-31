0251d30669316ef286d64552cf946edcb030fcdd
Tamim-369 <155313411+Tamim-369@users.noreply.github.com>
2023-12-31 10:21:50 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,8 @@
 
 cp -r polybar ~/.config/
 cp -r i3 ~/.config/
-cp -r Nordic-darker ~/.themes/
 cp bg2.jpg ~/Pictures/
+mkdir .themes
+cd .themes
+git clone https://github.com/EliverLara/Nordic.git
+cd ../
